6abe2a90457182c0e2633208f55c12eeb6334b15
auphelia <jakobapk@web.de>
2019-12-12 15:19:27 +0000

    [Execution] Changed clacl_tmem and calc_wmem according to the needed shape for hls code

--- src/finn/custom_op/fpgadataflow/convlayer_batch.py
+++ src/finn/custom_op/fpgadataflow/convlayer_batch.py
@@ -14,9 +14,6 @@ class ConvLayer_Batch(HLSCustomOp):
 
     def get_nodeattr_types(self):
         my_attrs = {
-            "backend": ("s", True, "fpgadataflow"),
-            "code_gen_dir": ("s", True, ""),
-            "executable_path": ("s", True, ""),
             "ConvKernelDim": ("i", True, 0),
             "IFMChannels": ("i", True, 0),
             "IFMDim": ("i", True, 0),
@@ -25,29 +22,39 @@ class ConvLayer_Batch(HLSCustomOp):
             "SIMD": ("i", True, 0),
             "PE": ("i", True, 0),
             "resType": ("s", True, ""),
-            "ActVal": ("i", False, 0),
             # FINN DataTypes for inputs, weights, outputs
             "inputDataType": ("s", True, ""),
             "weightDataType": ("s", True, ""),
             "outputDataType": ("s", True, ""),
             "binaryXnorMode": ("i", False, 0),
+            "noActivation": ("i", False, 0),
         }
         my_attrs.update(super().get_nodeattr_types())
         return my_attrs
 
     def calc_wmem(self):
-        ifmdim = self.get_nodeattr("IFMDim")
+        k = self.get_nodeattr("ConvKernelDim")
+        ifm_ch = self.get_nodeattr("IFMChannels")
+        ofm_ch = self.get_nodeattr("OFMChannels")
+        mw = k * k * ifm_ch
+        mh = ofm_ch
         pe = self.get_nodeattr("PE")
         simd = self.get_nodeattr("SIMD")
-        assert ifmdim % pe == 0
-        assert ifmdim % simd == 0
-        wmem = ifmdim * ifmdim // (pe * simd)
+        assert mh % pe == 0
+        assert mw % simd == 0
+        wmem = mw * mh // (pe * simd)
         return wmem
 
+
     def calc_tmem(self):
-        ifmdim = self.get_nodeattr("IFMDIM")
-        pe = self.get_nodeattr("PE")
-        return ifmdim // pe
+        if self.get_nodeattr("noActivation") == 1:
+            return 0
+        else:
+            ofm_ch = self.get_nodeattr("OFMChannels")
+            mh = ofm_ch
+            pe = self.get_nodeattr("PE")
+            return mh // pe
+
 
     def make_shape_compatible_op(self):
         pass
@@ -75,51 +82,54 @@ class ConvLayer_Batch(HLSCustomOp):
         o_bits = self.get_output_datatype().bitwidth()
         return o_bits * self.get_nodeattr("PE")
 
-    def execute_node(self, context, graph):
-        node = self.onnx_node
-        ifmdim = self.get_nodeattr("IFMDim")
-        simd = self.get_nodeattr("SIMD")
-        pe = self.get_nodeattr("PE")
-        sf = ifmdim // simd
-        nf = ifmdim // pe
+    def get_template_param_values(self):
+        ret = dict()
+        inp_hls_str = self.get_input_datatype().get_hls_datatype_str()
+        out_hls_str = self.get_output_datatype().get_hls_datatype_str()
+        inp_is_binary = self.get_input_datatype() == DataType.BINARY
+        out_is_binary = self.get_output_datatype() == DataType.BINARY
+        wt_is_binary = self.get_weight_datatype() == DataType.BINARY
+        bin_xnor_mode = self.get_nodeattr("binaryXnorMode") == 1
+        if (inp_is_binary or wt_is_binary) and (not bin_xnor_mode):
+            raise Exception("True binary (non-bipolar) inputs not yet supported")
+        inp_is_bipolar = self.get_input_datatype() == DataType.BIPOLAR
+        out_is_bipolar = self.get_output_datatype() == DataType.BIPOLAR
+        wt_is_bipolar = self.get_weight_datatype() == DataType.BIPOLAR
+        # reinterpret inp/wt as bipolar if bin_xnor_mode is iset
+        inp_is_bipolar = inp_is_bipolar or (inp_is_binary and bin_xnor_mode)
+        wt_is_bipolar = wt_is_bipolar or (wt_is_binary and bin_xnor_mode)
+        # fill in TSrcI and TWeightI
+        # TODO check these with Giulio
+        # TODO handle non-bipolar binary inputs
+        if inp_is_bipolar and wt_is_bipolar:
+            ret["TSrcI"] = "Recast<XnorMul>"
+            ret["TWeightI"] = "Identity"
+        elif (not inp_is_bipolar) and wt_is_bipolar:
+            ret["TSrcI"] = "Slice<%s>" % inp_hls_str
+            ret["TWeightI"] = "Recast<Binary>"
+        elif inp_is_bipolar and (not wt_is_bipolar):
+            ret["TSrcI"] = "Recast<Binary>"
+            ret["TWeightI"] = "Identity"
+        elif (not inp_is_bipolar) and (not wt_is_bipolar):
+            ret["TSrcI"] = "Slice<%s>" % inp_hls_str
+            ret["TWeightI"] = "Identity"
+        # fill in TDstI
+        if out_is_bipolar or out_is_binary:
+            ret["TDstI"] = "Identity"
+        else:
+            ret["TDstI"] = "Slice<%s>" % out_hls_str
+        return ret
 
-        # TODO ensure codegen dir exists
-        code_gen_dir = self.get_nodeattr("code_gen_dir")
-        # create a npy file fore each input of the node (in_ind is input index)
-        in_ind = 0
-        for inputs in node.input:
-            # it is assumed that the first input of the node is the data input
-            # the second input are the weights
-            # the third input are the thresholds
-            if in_ind == 0:
-                assert str(context[inputs].dtype) == "float32"
-                expected_inp_shape = (1, sf, simd)
-                reshaped_input = context[inputs].reshape(expected_inp_shape)
-                # flip SIMD (innermost) dimension of input tensor, there's some reversal
-                # going on somewhere with a mistmatch between npy and hls...
-                reshaped_input = np.flip(reshaped_input, -1)
-                if self.get_input_datatype() == DataType.BIPOLAR:
-                    # store bipolar activations as binary
-                    reshaped_input = (reshaped_input + 1) / 2
-                np.save(
-                    os.path.join(code_gen_dir, "input_{}.npy".format(in_ind)),
-                    reshaped_input,
-                )
-            elif in_ind > 2:
-                raise Exception("Unexpected input found for ConvLayer")
-            in_ind += 1
-        # execute the precompiled model
-        super().exec_precompiled_singlenode_model()
-        # load output npy file
-        super().npy_to_dynamic_output(context)
-        # reinterpret binary output as bipolar where needed
-        if self.get_output_datatype() == DataType.BIPOLAR:
-            out = context[node.output[0]]
-            out = 2 * out - 1
-            context[node.output[0]] = out
-        assert context[node.output[0]].shape == (1, nf, pe)
-        # reshape output to have expected shape
-        context[node.output[0]] = context[node.output[0]].reshape(1, ifmdim)
+    def get_hls_compatible_weight_tensor(self, orig_weight_matrix):
+        pass
+
+    def get_hls_compatible_threshold_tensor(self, orig_thres_matrix):
+        pass 
+    
+    def generate_params(self, model):
+        pass
+
+    def execute_node(self, context, graph):
 
     def global_includes(self):
         self.code_gen_dict["$GLOBALS$"] = ['#include "weights.hpp"']
@@ -148,16 +158,83 @@ class ConvLayer_Batch(HLSCustomOp):
         ]
 
     def read_npy_data(self):
-        pass
+        code_gen_dir = self.get_nodeattr("code_gen_dir")
+        dtype = self.get_input_datatype()
+        if dtype == DataType.BIPOLAR:
+            # use binary for bipolar storage
+            dtype = DataType.BINARY
+        elem_bits = dtype.bitwidth()
+        packed_bits = self.get_instream_width()
+        packed_hls_type = "ap_uint<%d>" % packed_bits
+        elem_hls_type = dtype.get_hls_datatype_str()
+        npy_type = "float"
+        npy_in = "%s/input_0.npy" % code_gen_dir
+        self.code_gen_dict["$READNPYDATA$"] = []
+        self.code_gen_dict["$READNPYDATA$"].append(
+            'npy2apintstream<%s, %s, %d, %s>("%s", in0);'
+            % (packed_hls_type, elem_hls_type, elem_bits, npy_type, npy_in)
+        ) 
 
     def strm_decl(self):
-        pass
+        self.code_gen_dict["$STREAMDECLARATIONS$"] = []
+        self.code_gen_dict["$STREAMDECLARATIONS$"].append(
+            'hls::stream<ap_uint<{}>> in0 ("in0");'.format(self.get_instream_width())
+        )
+        self.code_gen_dict["$STREAMDECLARATIONS$"].append(
+            'hls::stream<ap_uint<{}>> out ("out");'.format(self.get_outstream_width())
+        )
 
     def docompute(self):
-        pass
+        node = self.onnx_node
+        tmpl_args = self.get_template_param_values()
+        if self.calc_tmem() == 0:
+            odtype_hls_str = self.get_output_datatype().get_hls_datatype_str()
+            threshs = "PassThroughActivation<%s>()" % odtype_hls_str
+        else:
+            threshs = "threshs"
+        
+        self.code_gen_dict["$DOCOMPUTE$"] = [
+            """{}<ConvKernelDim1, IFMChannels1, IFMDim1, OFMChannels1, 
+            OFMDim1, SIMD1, PE1, {}, {}, {}>
+            (in0, out, weights, {}, numReps, {});""".format(
+                node.op_type,
+                tmpl_args["TSrcI"],
+                tmpl_args["TDstI"],
+                tmpl_args["TWeightI"],
+                threshs,
+                self.get_nodeattr("resType"),
+            )
+        ]
+
 
     def dataoutstrm(self):
-        pass
+        code_gen_dir = self.get_nodeattr("code_gen_dir")
+        dtype = self.get_output_datatype()
+        if dtype == DataType.BIPOLAR:
+            # use binary for bipolar storage
+            dtype = DataType.BINARY
+        elem_bits = dtype.bitwidth()
+        packed_bits = self.get_outstream_width()
+        packed_hls_type = "ap_uint<%d>" % packed_bits
+        elem_hls_type = dtype.get_hls_datatype_str()
+        npy_type = "float"
+        npy_out = "%s/output.npy" % code_gen_dir
+        nf = int(self.get_nodeattr("MH") / self.get_nodeattr("PE"))
+        shape = (1, nf, self.get_nodeattr("PE"))
+        shape_cpp_str = str(shape).replace("(", "{").replace(")", "}")
+
+        self.code_gen_dict["$DATAOUTSTREAM$"] = [
+            'apintstream2npy<%s, %s, %d, %s>(out, %s, "%s");'
+            % (
+                packed_hls_type,
+                elem_hls_type,
+                elem_bits,
+                npy_type,
+                shape_cpp_str,
+                npy_out,
+            )
+        ]
+
 
     def save_as_npy(self):
-        pass
+        self.code_gen_dict["$SAVEASCNPY$"] = []
